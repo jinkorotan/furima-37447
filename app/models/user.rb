6ed2bc25cf1eac361311d2a16d09a00b4b17e78c
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-08 22:11:40 +0900

    各種バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)(?=.*?[\W_])[!-~]{8,}+\z/
-validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
+ PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+ validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+         
+
  
   validates :nickname,            presence: true
   validates :kanji_first_name,    presence: true
@@ -12,4 +15,17 @@ validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :kana_first_name,     presence: true
   validates :kana_last_name,      presence: true
   validates :birthday,            presence: true
+ 
+
+
+  with_options presence: true do
+    # ひらがな、カタカナ、漢字のみ許可する
+    validates :kanji_first_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :kanji_last_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+  # カタカナのみ許可する
+  validates :kana_first_name, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :kana_last_name, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
+end
+
 end
