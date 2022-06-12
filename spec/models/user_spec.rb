9806dca460ff08f563494730cc8a48afdf6f64d7
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-12 12:24:42 +0900

    単体テストコード4
    beforeで共通した記述を切り出す

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
+before do
+  @user = FactoryBot.build(:user)
+end
+
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do  
-      user =FactoryBot.build(:user)  # Userのインスタンス生成
-      user.nickname = ''  # nicknameの値を空にする
+      @user.nickname = ''
       user.valid?
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.email = ''  # emailの値を空にする
+      @user.email = ''
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end   
@@ -24,36 +26,31 @@ RSpec.describe User, type: :model do
     end
 
     it 'お名前(全角)の名前が空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.kanji_first_name = ''  # kanji_first_name の値を空にする
+      @user.kanji_first_name = ''
       user.valid?
       expect(user.errors.full_messages).to include("Kanji first name can't be blank")
     end
 
     it 'お名前(全角)の苗字が空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.kanji_last_name = ''  # kanji_last_nameの値を空にする
+      @user.kanji_last_name = ''
       user.valid?
       expect(user.errors.full_messages).to include("Kanji last name can't be blank")
     end
 
     it 'お名前カナ(全角)の名前が空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.kana_first_name = ''  # kanji_first_name の値を空にする
+      @user.kana_first_name = ''
       user.valid?
       expect(user.errors.full_messages).to include("Kana first name can't be blank")
     end
 
     it 'お名前カナ(全角)の苗字が空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.kana_last_name = ''  # kana_last_nameの値を空にする
+      @user.kana_last_name = ''
       user.valid?
       expect(user.errors.full_messages).to include("Kana last name can't be blank")
     end
 
     it '生年月日が空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.birthday = ''  # birthdayの値を空にする
+      @user.birthday = ''
       user.valid?
       expect(user.errors.full_messages).to include("Birthday can't be blank")
     end
