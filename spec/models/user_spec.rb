3d463db7ff5668dadc2f0c5a12937b7b213b7c08
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-12 12:10:38 +0900

    単体テストコード2
    既に登録され得ているemailを重複して登録できない

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,16 @@ RSpec.describe User, type: :model do
       user.email = ''  # emailの値を空にする
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
-    end    
+    end   
+
+    it '既に登録され得ているemailを重複して登録できない' do
+      @user.save
+      @user2.=FactoryBot.build(:user)
+      @user2.email=@user.email
+      @user2.valid?
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+
     it 'お名前(全角)の名前が空では登録できない' do
       user = FactoryBot.build(:user)  # Userのインスタンス生成
       user.kanji_first_name = ''  # kanji_first_name の値を空にする
