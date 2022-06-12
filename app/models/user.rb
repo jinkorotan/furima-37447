d0c4d5b00e173161d88ca9fe05701e73c1415f1e
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-12 11:31:49 +0900

    正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,13 +19,11 @@ class User < ApplicationRecord
 
 
   with_options presence: true do
-    # ひらがな、カタカナ、漢字のみ許可する
+  #ひらがな、カタカナ、漢字のみ許可する
     validates :kanji_first_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :kanji_last_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-  # カタカナのみ許可する
-  validates :kana_first_name, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :kana_last_name, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+   #カタカナのみ許可する
+    validates :kana_first_name, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :kana_last_name, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
   end
-end
-
 end
