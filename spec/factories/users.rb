7d5c725f20dedec0241be373799627e9c71bb042
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-14 15:52:59 +0900

    修正2-5
    テストコードFakerはやめた

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     email                 { Faker::Internet.free_email }
     password              { '1a' + Faker::Internet.password(min_length: 6,mix_case: false) }
     password_confirmation { password }
-    kanji_first_name      { Faker::Name.first_name }
-    kanji_last_name       { Faker::Name.last_name }
+    kanji_first_name      { '山田' }
+    kanji_last_name       {'太郎' }
     kana_first_name       {'ヤマダ'}
     kana_last_name        {'タロウ'}
     birthday              { Faker::Date.birthday }
