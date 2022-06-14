6b817d179894d0be2dcbc9068c0c323cb3fd98b2
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-14 18:36:38 +0900

    バリデーション設定
    READMEにあるもののみ。後で商品画像等について追加する

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+
+  validates :name,                    presence: true
+  validates :price,                   presence: true
+  validates :description,             presence: true
+  validates :category_id,             presence: true
+  validates :condition_id,            presence: true
+  validates :shipping_charge_id,      presence: true
+  validates :prefecture_id,           presence: true
+  validates :days_to_ship_id,         presence: true
+ 
 end
