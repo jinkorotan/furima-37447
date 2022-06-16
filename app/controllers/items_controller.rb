026fb93f331b0d66e63b32ab2955667db71c8e71
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-16 21:50:06 +0900

    エラーハンドリング
    入力に問題がある状態で「出品する」ボタンが押された場合、情報は保存されず、出品ページに戻りエラーメッセージが表示される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,9 +11,27 @@ class ItemsController < ApplicationController
      #新規作成ボタンをクリックしたらnewアクションが働く
   end
 
-  # private
-#   def item_params
-#     params.require(:item).permit(:image).merge(user_id: current_user.id)
-#   end
+
+  def create
+    @item= Item.new(item_params)
+
+    if @item .save
+      #保存に成功したら
+      redirect_to root_path
+      #redirect_toメソッドでルートパスにリダイレクト
+    else
+      render :new
+      #保存が失敗した場合はrenderメソッドでitem_/new.html.erbのページを表示
+    end
+
+  end
+
+  private
+    def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+      #imageはviewで入力したもの
+      #user_idは勝手に付与されるもの
+      #勝手に付与されたuser_idをviewで入力した物とくっつけて(mergeして)操作したい
+    end
 
  end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,6 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :name,                    presence: true
-  validates :price,                   presence: true
   validates :description,             presence: true
   validates :image,                   presence: true
  
@@ -20,4 +19,8 @@ validates :condition_id, numericality: { other_than: 1 , message: "can't be blan
 validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank" } 
 validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" } 
 validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank" } 
+
+ with_options  presence: true, format: {with: /\A[a-z0-9]+\z/i,} do
+    validates :price, numericality{only_integer:true, greater_than_or_equal_to:300,less_than_or_equal_to: 9_999_999 },
+               presence: {message: "can't be blank"}
 end
