e2b70aa678d5fc6aa4a91f77abb834097d5361ed
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-23 18:56:33 +0900

    ストロングパラメータの記述見直し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,6 @@ class ItemsController < ApplicationController
 
   private
       def item_params  
-        params.require(:item).permit(:name,:condition_id, :image, :description, :category_id , :category_id , :shipping_charge_id , :prefecture_id , :prefecture_id , :days_to_ship_id , :price )
+        params.require(:item).permit(:name,:condition_id, :image, :description, :category_id , :shipping_charge_id , :prefecture_id ,:days_to_ship_id , :price ).merge(user_id: current_user.id)
       end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -23,7 +23,7 @@ class Item < ApplicationRecord
     validates :days_to_ship_id
   end
 
-  with_options presence: true, format: { with: /\A[a-z0-9]+\z/i } do
+  with_options presence: true do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
                       presence: { message: "can't be blank" }
   end
