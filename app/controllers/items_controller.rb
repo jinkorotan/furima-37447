59b0d14d515f60c33c8c2772807eac1936e5dfd1
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-24 18:44:45 +0900

    細かい修正
    日時が新しい順に表示される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   # ログインしていないユーザーをログインページの画面に促す
   def index
-    @item =Item.all.order("created_at DESC")
+    @item =Item.all
+   #.allで上から順に取ってくるということになる
   end
 
   def new
