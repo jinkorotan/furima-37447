606eeccfb1a4c2657e75582d7051380881950d52
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-26 21:01:54 +0900

    ルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def edit
-  #   @item = Item.find(params[:id])
-  # end
+  def edit
+    @item = Item.find(params[:id])
+  end
 
   def show
     @item = Item.find(params[:id])
