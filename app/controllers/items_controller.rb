155e2b0431c9a2b5127e3af4886f557ad5492311
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-29 16:16:48 +0900

    destroyメソッド設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,10 +43,10 @@ class ItemsController < ApplicationController
     end 
   end 
 
-  # def destroy
-  #     item =Item.find(params[:id])
-  #     item.destroy
-  # end  
+  def destroy
+      item =Item.find(params[:id])
+      item.destroy
+  end  
 
   private
     def item_params
