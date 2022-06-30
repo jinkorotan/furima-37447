3acaf8b43090fb48a2db64d22f748b641781c893
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-06-30 15:36:22 +0900

    order_shipping.rb記述
    form_withメソッドに対応する機能とバリデーションを行う機能を、作成したクラスに持たせる

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,5 +1,5 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :user, :prefecture,:item,:item,
+  attr_accessor :user, :prefecture,:post_code,:prefecture_id,:municipality,:municipality,:address,:building_name,:phone_number,:order
 
 end
