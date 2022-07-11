badf3e8b3c8cefda46bc3c24a84b9d4c1a7299f5
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-07-11 15:48:16 +0900

    unlessをifに修正
     if current_user == @item.user
          redirect_to root_path
        end

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,18 +2,19 @@ class OrdersController < ApplicationController
 
   before_action :authenticate_user!
    # ログインしていないユーザーをログインページの画面に促す
+   before_action :set_item,  only: [:index, :create]
+   before_action :move_to_root_path, only: [:index, :create]
+   before_action :sold_out_root_path, only: [:index, :create]
 
   def index
    #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
    #order/indexに使いたい情報をここで@itemに入れる
    @order_shipping = OrderShipping.new
-   @item = Item.find(params[:item_id])
    #２重ハッシュの空の箱ができた
   end
   
   # order_shippingの保存
   def create
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_shipping_params)
 
     # if @order_shipping.valid?
@@ -26,7 +27,7 @@ class OrdersController < ApplicationController
       )
 
       @order_shipping.save
-      #order_shippingモデルのsaveを呼び出すs
+      #order_shippingモデルのsaveを呼び出す
       #フォームオブジェクトの保存
       return redirect_to root_path
     # else
@@ -38,6 +39,29 @@ class OrdersController < ApplicationController
   def order_shipping_params
     params.require(:order_shipping).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number,:token).merge(user_id: current_user.id, item_id: params[:item_id]  ) 
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+
+  def move_to_root_path
+
+    if current_user == @item.user
+      redirect_to root_path
+    end 
+  end
+  #現在ログインしているユーザーが商品を出品ユーザーではなかった時トップページに遷移する
+
+  def sold_out_root_path 
+    binding.pry
+    if  @item.order.present?
+      redirect_to root_path
+    end 
+   #sold outだったらトップページに遷移する
+  end
 end
+
+
 
 
