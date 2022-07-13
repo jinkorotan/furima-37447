5aaa4b012c34e70127ed7dfefddab70d61ce5594
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-07-13 18:01:52 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
                                       keys: [:nickname, :kanji_last_name, :kanji_first_name, :kana_last_name, :kana_last_name,
                                              :kana_first_name, :birthday])
   end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
+  
 end
